47c23e5869916809227a2aae0fa1170699001222
kamal <skamalkumar@gmail.com>
2022-08-12 21:48:18 +0530

    first commit

--- django3-personal-portfolio-master/personal_portfolio/settings.py
+++ django3-personal-portfolio-master/personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'kamalpersonal.herokuapp.com']
 
 
 # Application definition
